f17d43b2cabc0c6e7dbb4e23b9a908bc2a9386ee
Luis Tony Martinez <lmartine@ed2.ib0.cm.hpc.nrel.gov>
2020-07-13 20:09:42 -0600

    Updated cfd cases for output, num of actuator points and density

--- Parametric_RPM_CFD/combine_eup_files.sh
+++ Parametric_RPM_CFD/combine_eup_files.sh
@@ -4,6 +4,6 @@ module load trilinos
 
 for item in CT_0.10  CT_0.30  CT_0.50  CT_0.70  CT_0.90  CT_1.10  CT_1.30  CT_1.50  CT_1.70  CT_1.90
 do
-  cd /lustre/eaglefs/scratch/lmartine/high-thrust-ad/Parametric_Pitch_CFD/$item/plane_sampling_mean
+  cd /lustre/eaglefs/scratch/lmartine/high-thrust-ad/Parametric_RPM_CFD/$item/plane_sampling_mean
   epu -auto sampling.exo.180.000
 done
