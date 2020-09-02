bd8d838d4600cb3aee01579cac094ffef59a90b7
Luis Tony Martinez <lmartine@ed2.ib0.cm.hpc.nrel.gov>
2020-09-02 17:15:39 -0600

    rsync data script

---
+++ rsync_all.sh
@@ -0,0 +1,1 @@
+rsync -mzarv --include="*/" --include="Parametric_*/CT_*/*T1.out" --include="Parametric_*/CT_*/plane_sampling_mean/sampling.exo" --exclude="*" . /projects/isda/tony/high-thrust-results
